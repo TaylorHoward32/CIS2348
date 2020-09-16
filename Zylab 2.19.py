5de270630bee9f6214c92611ea41513e904f1323
TaylorHoward32 <tlhowar2@cougarnet.uh.edu>
2020-09-16 11:56:40 -0500

    Zylab 2.19

---
+++ Zylab 2.19.py
@@ -0,0 +1,52 @@
+#TaylorHoward
+#1649552
+#Zylab 2.19
+
+lemon_juice = float(input('Enter the amount of lemon juice (in cups):'))
+water = float(input('Enter amount of water (in cups):'))
+nectar = float(input('Enter amount of agave nectar (in cups):'))
+servings = float(input('How many servings this makes?'))
+
+print('Lemonade ingredients - yields', '{:.2f}'.format(servings), 'servings')
+print('{:.2f}'.format(lemon_juice), 'cup(s) lemon juice')
+print('{:.2f}'.format(water), 'cup(s) water')
+print('{:.2f}'.format(nectar), 'cup(s) of agave nectar')
+
+servings = float(input('/nHow many servings would you like to make?'))
+print('/nLemondae ingredients - yields', '{:.2f}'.format(servings), 'servings')
+lemon_juice = lemon_juice * 8
+water = water * 8
+nectar = nectar * 8
+print('[:.2f}'.format(lemon_juice), 'cups lemon juice')
+print('{:.2f}'.format(water), 'cups of water')
+print('{:.2f}'.format(nectar), 'cups of agave nectar')
+
+
+servings = float(input('How many servers would you like to make?'))
+print('Lemonade ingredients - yields', '{:.2f}'.format(servings), 'servings')
+lemon_juice = lemon_juice / 16
+water = water / 16
+nectar = nectar / 16
+print('{:.2f}'.format(lemon_juice), 'cups of lemon juice')
+print('{:.2f}'.format(water), 'cups of water')
+print('{:.2f}'.format(nectar), 'cups of agave nectar')
+
+
+ servings = float(input('How many servings would you like to make?'))
+print('Lemonade ingredients - yields', '{:.2f}'.format(servings), 'servings')
+lemon_juice = lemon_juice * 16
+water = water * 16
+nectar = nectar * 16
+print('{:.2f}'.format(lemon_juice), 'cups of lemon juice')
+print('{:.2f}'.format(water), 'cups of water')
+print('{:.2f}'.format(nectar), 'cups of agave nectar')
+
+
+ servings = float(input('How many servings would you like to make?'))
+print('Lemonade ingredients - yields', '{:.2f}'.format(servings), 'servings')
+lemon_juice = lemon_juice / 16
+water = water / 16
+nectar = nectar / 16
+print('{:.2f}'.format(lemon_juice), 'gallons of lemon juice')
+print('{:.2f}'.format(water), 'gallons of water')
+print('{:.2f}'.format(nectar), 'gallons of agave nectar')
