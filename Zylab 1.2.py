16f84fcd640cb9937b11d88b04eb789182613c89
TaylorHoward32 <tlhowar2@cougarnet.uh.edu>
2020-09-16 01:59:28 -0500

    Homework #1 , Birthday Code

---
+++ Zylab 1.2.py
@@ -0,0 +1,10 @@
+#TaylorHoward
+#September 14, 2020
+#CIS2348
+#Zylab 1.2
+
+userNum = int(input()) userNumSquared = userNum + userNum
+# Bug here; fix it when instructed
+
+print(userNumSquared, end=' ')
+# Output formatting issue here; fix it when instructed
