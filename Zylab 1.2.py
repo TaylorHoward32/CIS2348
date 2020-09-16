5d5c3ae75c6cfc67baefcde42b33ab4700e2b616
TaylorHoward32 <tlhowar2@cougarnet.uh.edu>
2020-09-16 11:56:16 -0500

    Zylab 1.20

--- Zylab 1.2.py
+++ Zylab 1.2.py
@@ -1,10 +1,8 @@
 #TaylorHoward
-#September 14, 2020
-#CIS2348
-#Zylab 1.2
+#1649552
+#Zybooks 1.12
 
-userNum = int(input()) userNumSquared = userNum + userNum
-# Bug here; fix it when instructed
+userNum1 = int(input('Enter Integer'))
+userNumSquared = userNum1 * userNum1 # Bug here; fix it when instructed
 
-print(userNumSquared, end=' ')
-# Output formatting issue here; fix it when instructed
+print(userNumSquared, end='\n',) # Output formatting issue here; fix it when instructed
