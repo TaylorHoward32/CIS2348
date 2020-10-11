070fc1f9ab998cfae88701ce23092b8b757f45ca
TaylorHoward32 <tlhowar2@cougarnet.uh.edu>
2020-10-11 13:54:59 -0500

    Homework 2

--- Homework 2.py
+++ Homework 2.py
@@ -1,1 +1,29 @@
+#TaylorHoward
+#10-09-2020
+#CIS2348-14654
+#Homework #2
+
+#list the months out with the corresponding number
+month_list= {"January":"1","February":"2","March":"3","April":"4","May":"5",
+"June":"6","July":"7","August":"8","September":"9","October":"10",
+"November":"11","December":"12"}
+
 #
+for each in:
+    if each!="-1":
+        lis=each.split()
+        if len(lis)>=3:
+            month=lis[0]
+            day=lis[1]
+            year=lis[2]
+
+            # How to display the answer with separating with commas or slashes
+            if month.lower() in month_list:
+                comma=day[-1]
+                if comma==',':
+                    day=day[:len(day)-1]
+                    month_number=month_list[month.lower()]
+                    ans=month_number+"/"+day+"/"+year
+
+
+
